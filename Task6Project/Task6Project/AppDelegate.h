d7129c8e10a81f87affc4233b184ee5c5de4f324
Eduard Ivash <eduardivash@gmail.com>
2020-06-20 11:51:04 +0300

    Initial commit

---
+++ Task6Project/Task6Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Task6Project
+//
+//  Created by Эдуард on 6/20/20.
+//  Copyright © 2020 Eduard Ivash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Task6Project/Task6Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task6Project
+//
+//  Created by Эдуард on 6/20/20.
+//  Copyright © 2020 Eduard Ivash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
